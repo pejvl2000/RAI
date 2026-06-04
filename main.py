899c620250b6a4061cf11b409a8cf95945c8c96f
tokenshu <hajnik217@gmail.com>
2026-06-04 16:15:45 +0200

    Update main.py
    unique ant id

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ colony_memory = {
     }
 }
 
+next_ant_id = 1
+
 # ---------------- WORLD ----------------
 
 def WorldGen(size_x, size_y, seed_id):
@@ -252,7 +254,10 @@ class Ant:
         self.attack = ANT_ATTACK
         self.vision_radius = VISION_RAD
         self.combat_lock = 0
-        self.ant_id = random.randint(1, 999)
+        global next_ant_id
+
+        self.ant_id = next_ant_id
+        next_ant_id += 1
         self.wait_ticks = 0
 
         # sdílení paměti mezi mravenci stejné kolonie (navštívené, objevené a zacílené pozice)
